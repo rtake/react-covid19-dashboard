edfcc036111f13638bcfdaeb3ad6fc7b1e73c132
rtake <rtake1110@gmail.com>
2023-08-01 18:19:49 +0900

    lineの色を修正

--- src/components/LinePlot.tsx
+++ src/components/LinePlot.tsx
@@ -7,6 +7,7 @@ const data = {
         {
             label: "Demo Line plot",
             backgroundColor: "#008080",
+            borderColor: "#7fffd4",
             pointBorderWidth: 10,
             data: [5, 6, 9, 15, 30, 40, 80]
         }
