d4c42929c88736fc42fe9c41df0de81e94446536
rtake <rtake1110@gmail.com>
2023-08-01 19:51:31 +0900

    containerをネストするサンプルコードを追加

--- src/components/MaterialUI.tsx
+++ src/components/MaterialUI.tsx
@@ -63,7 +63,7 @@ const MaterialUI : React.FC = () => {
             alignItems="flex-end" 
             spacing={2}
         >
-            <Grid item xs={1}>
+            <Grid item xs={1} justifyContent='space-around'>
                 <Paper className={classes.paperStyle}>xs=1</Paper>
             </Grid>
             <Grid item xs={1}>
@@ -71,6 +71,25 @@ const MaterialUI : React.FC = () => {
             </Grid>
             <Grid item xs={1}>
                 <Paper className={classes.paperStyle}>xs=1</Paper>
+            </Grid>
+        </Grid>
+
+        <Grid container spacing={3} direction='column'>
+            <Grid item xs={12} container>
+                <Grid item xs={2}>Demo</Grid>
+                <Grid item xs={8}></Grid>
+                <Grid item xs={2}>Test</Grid>
+            </Grid>
+            <Grid item xs={12} container justifyContent="space-around">
+                <Grid item xs={3}>
+                    The Auto-layout makes the items equitably share the available space. That also means you can set the width of one item and the others will automatically resize around it.
+                </Grid>
+                <Grid item xs={3}>
+                    The Auto-layout makes the items equitably share the available space. That also means you can set the width of one item and the others will automatically resize around it.
+                </Grid>
+                <Grid item xs={3}>
+                    The Auto-layout makes the items equitably share the available space. That also means you can set the width of one item and the others will automatically resize around it.
+                </Grid>
             </Grid>
         </Grid>
     </div>
